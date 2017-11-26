21e43b12ac05f6b1c0bd8081f1bcc292af36e2de
Jason Thrasher <jasonthrasher@gmail.com>
2017-11-26 11:42:45 -0800

    renamed server.sh

--- minecraft_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Runs the server for minecraft
-# Download the server jar from: https://minecraft.net/en-us/download/server
-#
-
-java -Xmx1024M -Xms1024M -jar minecraft_server.1.12.2.jar nogui
